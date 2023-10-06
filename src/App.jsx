a257f9f87bf81a8bbc81fc95f0b2947436e2438f
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2023-10-06 21:35:42 +0530

    Adding files 📄

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Courses from "./pages/courses/Courses";
+import Roadmap from "./pages/roadmap/Roadmap";
+import TestSeries from "./pages/testSeries/TestSeries";
+import Mentor from "./pages/mentor/Mentor";
+import FinancialResources from "./pages/financialResources/FinancialResources";
+import Contact from "./pages/contact/Contact";
+import Profile from "./pages/profile/Profile";
+import SignIn from "./pages/signin/SignIn";
+import SignUp from "./pages/signup/SignUp";
+import "./App.css";
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/" index element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/courses" element={<Courses />} />
+                <Route path="/roadmap" element={<Roadmap />} />
+                <Route path="/test-series" element={<TestSeries />} />
+                <Route path="/mentor" element={<Mentor />} />
+                <Route
+                    path="/financial-resources"
+                    element={<FinancialResources />}
+                />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import performancePicture from "../../assets/performance.png";
+import "./home.css";
+
+const Home = () => {
+    return (
+        <div>
+            <div id="hero-section">
+                <div class="student-images-section">
+                    <div id="student-images">
+                        <p>Hello student</p>
+                    </div>
+                </div>
+                <div class="performance-image-section">
+                    <h1>My Learning </h1>
+                    <p>Track your learning process</p>
+                    <div id="performance-image-container">
+                        <img src={performancePicture} alt="" />
+                    </div>
+                </div>
+
+                <div id="profile-page">
+                    <i
+                        id="profile-back-btn"
+                        class="fa-solid fa-xmark fa-lg"
+                    ></i>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
